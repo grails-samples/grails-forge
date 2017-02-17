fb43af27d19779b093abd1b22f136aee01c71e3f
sdelamo <sergio.delamo@softamo.com>
2017-02-17 11:47:38 +0100

    Deploy to Cloud Found with Travis

--- travis-build.sh
+++ travis-build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 set -e
 rm -rf *.zip
-./gradlew clean check
+
+./gradlew clean check || EXIT_STATUS=$?
+
+if [[ $EXIT_STATUS ]]; then
+
+    if [[ ( $TRAVIS_BRANCH == master || $TRAVIS_TAG == prod_* ) && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+        echo "Publishing to PWS"
+
+        ./grailsw war || EXIT_STATUS=$?
+
+        if [[ $EXIT_STATUS ]]; then
+            ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD clean assemble cfPush || EXIT_STATUS=$i
+        fi
+
+    fi
+fi
+exit $EXIT_STATUS
