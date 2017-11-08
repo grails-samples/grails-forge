caa8673e251ecb1162ff1110af0a8eab0afcb684
sdelamo <sergio.delamo@softamo.com>
2017-11-08 19:41:37 +0100

    Revert "Ignore failing tests"
    This reverts commit 568fda4be686bb85eef456ce85f68a4d942dce9e.

--- src/integration-test/groovy/appgenerator/GenerateControllerIntegrationSpec.groovy
+++ src/integration-test/groovy/appgenerator/GenerateControllerIntegrationSpec.groovy
@@ -1,6 +1,5 @@
 package appgenerator
 
-import grails.plugins.rest.client.RestBuilder
 import grails.testing.mixin.integration.Integration
 import spock.lang.Specification
 
@@ -9,33 +8,25 @@ import static org.springframework.http.HttpStatus.NO_CONTENT
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
 
 @Integration
-class GenerateControllerIntegrationSpec extends Specification {
+class GenerateControllerIntegrationSpec extends Specification implements RestSpec {
 
     void "test validate"() {
-        given:
-        RestBuilder restBuilder = new RestBuilder()
-
-        when:
-        def resp = restBuilder.post("http://localhost:${serverPort}/validate") {
+        def resp = restBuilder().post("$baseUrl/validate") {
             contentType('application/x-www-form-urlencoded')
             body('name=foo&version=3.2.3')
         }
 
-        then:"The response is correct"
+        expect:"The response is correct"
         resp.status == NO_CONTENT.value()
     }
 
     void "test validate unsupported version"() {
-        given:
-        RestBuilder restBuilder = new RestBuilder()
-
-        when:
-        def resp = restBuilder.post("http://localhost:${serverPort}/validate") {
+        def resp = restBuilder().post("$baseUrl/validate") {
             contentType('application/x-www-form-urlencoded')
             body('name=foo&version=3.2.0')
         }
 
-        then:"The response is correct"
+        expect:"The response is correct"
         resp.status == UNPROCESSABLE_ENTITY.value()
         resp.headers[CONTENT_TYPE] == ['application/json;charset=UTF-8']
         resp.json.version == "The version specified is not supported"
@@ -43,13 +34,9 @@ class GenerateControllerIntegrationSpec extends Specification {
     }
 
     void "test validate validation errors"() {
-        given:
-        RestBuilder restBuilder = new RestBuilder()
+        def resp = restBuilder().post("$baseUrl/validate")
 
-        when:
-        def resp = restBuilder.post("http://localhost:${serverPort}/validate")
-
-        then:"The response is correct"
+        expect:"The response is correct"
         resp.status == UNPROCESSABLE_ENTITY.value()
         resp.headers[CONTENT_TYPE] == ['application/json;charset=UTF-8']
         resp.json.version == "You must specify a version for your project"
--- src/integration-test/groovy/appgenerator/IndexControllerIntegrationSpec.groovy
+++ src/integration-test/groovy/appgenerator/IndexControllerIntegrationSpec.groovy
@@ -1,7 +1,5 @@
 package appgenerator
 
-import grails.plugins.rest.client.RestBuilder
-import grails.plugins.rest.client.RestResponse
 import grails.testing.mixin.integration.Integration
 import spock.lang.Specification
 
@@ -9,18 +7,14 @@ import static grails.web.http.HttpHeaders.CONTENT_TYPE
 import static org.springframework.http.HttpStatus.OK
 
 @Integration
-class IndexControllerIntegrationSpec extends Specification {
+class IndexControllerIntegrationSpec extends Specification implements RestSpec {
 
     void "test index with curl"() {
-        given:
-        RestBuilder restBuilder = new RestBuilder()
-
-        when:
-        RestResponse resp = restBuilder.get("http://localhost:${serverPort}/") {
+        def resp = restBuilder().get("$baseUrl/") {
             header("User-Agent", "curl")
         }
 
-        then: 'The response is correct'
+        expect:"The response is correct"
         resp.status == OK.value()
         resp.headers[CONTENT_TYPE] == ['text/plain']
         resp.text.startsWith('Grails Application Forge')
