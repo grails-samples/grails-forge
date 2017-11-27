60e9c0ebe48e4f7d9e4a1c8396566b1f58384a0e
sdelamo <sergio.delamo@softamo.com>
2017-11-27 16:30:09 +0100

    dont autoStartup Quartz in test environment

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,34 @@
+import grails.util.Environment
+import groovy.transform.CompileStatic
+import org.quartz.Scheduler
+
 class BootStrap {
+    Scheduler quartzScheduler
 
     def init = { servletContext ->
+        if ( Environment.current == Environment.DEVELOPMENT ) {
+            configureForDevelopment()
+        } else if ( Environment.current == Environment.TEST ) {
+            configureForTest()
+        } else if ( Environment.current == Environment.PRODUCTION ) {
+            configureForProduction()
+        }
     }
+
     def destroy = {
+    }
+
+    @CompileStatic
+    void configureForTest() {
+    }
+
+    @CompileStatic
+    void configureForDevelopment() {
+        quartzScheduler.start()
+    }
+
+    @CompileStatic
+    void configureForProduction() {
+        quartzScheduler.start()
     }
 }
