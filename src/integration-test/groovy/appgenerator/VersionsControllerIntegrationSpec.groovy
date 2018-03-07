2a69bb331831ea1b120f539fe49452c9cdea4d7a
Sergio del Amo <sergio.delamo@softamo.com>
2018-03-07 10:07:54 +0100

    simplify tests to make it more tolerant to failure
    this tests was fragile when new versiosn were being added

--- src/integration-test/groovy/appgenerator/VersionsControllerIntegrationSpec.groovy
+++ src/integration-test/groovy/appgenerator/VersionsControllerIntegrationSpec.groovy
@@ -20,12 +20,35 @@ class VersionsControllerIntegrationSpec extends Specification implements RestSpe
     }
 
     void "test appData"() {
+        when:
         RestResponse resp = get('/appData')
 
-        expect:"The response is correct"
+        then:"The response is correct"
         resp.json.size() > 0
         resp.status == OK.value()
-        resp.text.startsWith('[{"version":"3.1.13","profiles":[{"version":"3.1.9","features":[{"required":true,"defaultFeature":false,"description":"Adds Asset Pipeline to a Grails project","name":"asset-pipeline"},{"required":false,"defaultFeature":true,"description":"Adds GORM for Hibernate to the project","name":"hibernate"},{"required":true,"defaultFeature":false,"description":"Adds support for JSON Views to the project","name":"json-views"},{"required":false,"defaultFeature":false,"description":"Adds LESS Transpiler Asset Pipeline to a Grails project","name":"less-asset-pipeline"},{"required":false,"defaultFeature":false,"description":"Adds support for Markup Views to the project","name":"markup-views"},{"required":false,"defaultFeature":false,"description":"Adds GORM for MongoDB to the project","name":"mongodb"},{"required":false,"defaultFeature":false,"description":"Adds GORM for Neo4j to the project","name":"neo4j"},{"required":false,"defaultFeature":false,"description":"Adds Spring Security REST to the project","name":"security"}],"description":"A profile for creating applications using AngularJS","name":"angular"},{"version":"3.1.9","features":[{"required":false,"defaultFeature":false,"description":"Adds Asset Pipeline to a Grails project","name":"asset-pipeline"},{"required":false,"defaultFeature":true,"description":"Adds GORM for Hibernate to the project","name":"hibernate"},{"required":true,"defaultFeature":false,"description":"Adds support for JSON Views to the project","name":"json-views"},{"required":false,"defaultFeature":false,"description":"Adds LESS Transpiler Asset Pipeline to a Grails project","name":"less-asset-pipeline"},{"required":false,"defaultFeature":false,"description":"Adds support for Markup Views to the project","name":"markup-views"},{"required":false,"defaultFeature":false,"description":"Adds GORM for MongoDB to the project","name":"mongodb"},{"required":false,"defaultFeature":false,"description":"Adds GORM for Neo4j to the project","name":"neo4j"},{"required":false,"defaultFeature":false,"description":"Adds Spring Security REST to the project","name":"security"}],"description":"Profile for REST API applications","name":"rest-api"},{"version":"3.1.9","features":[{"required":false,"defaultFeature":true,"description":"Adds Asset Pipeline to a Grails project","name":"asset-pipeline"},{"required":false,"defaultFeature":true,"description":"Adds GORM for Hibernate to the project","name":"hibernate"},{"required":false,"defaultFeature":false,"description":"Adds support for JSON Views to the project","name":"json-views"},{"required":false,"defaultFeature":false,"description":"Adds LESS Transpiler Asset Pipeline to a Grails project","name":"less-asset-pipeline"},{"required":false,"defaultFeature":false,"description":"Adds support for Markup Views to the project","name":"markup-views"},{"required":false,"defaultFeature":false,"description":"Adds GORM for MongoDB to the project","name":"mongodb"},{"required":false,"defaultFeature":false,"description":"Adds GORM for Neo4j to the project","name":"neo4j"}],"description":"Profile for Web applications","name":"web"}]}')
-    }
 
+        when:
+        List<String> versions = resp.json.collect { it.version }
+
+        then:
+        versions.contains('3.1.13')
+        versions.contains('3.1.13')
+        versions.contains('3.1.14')
+        versions.contains('3.1.15')
+        versions.contains('3.1.16')
+        versions.contains('3.2.2')
+        versions.contains('3.2.3')
+        versions.contains('3.2.4')
+        versions.contains('3.2.5')
+        versions.contains('3.2.6')
+        versions.contains('3.2.7')
+        versions.contains('3.2.8')
+        versions.contains('3.2.9')
+        versions.contains('3.2.10')
+        versions.contains('3.2.11')
+        versions.contains('3.2.12')
+        versions.contains('3.3.0')
+        versions.contains('3.3.1')
+        versions.contains('3.3.2')
+    }
 }
