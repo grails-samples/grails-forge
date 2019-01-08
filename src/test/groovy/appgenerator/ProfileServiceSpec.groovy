6a798eaff44c7c5db17172eb0f254c055361965f
sdelamo <sergio.delamo@softamo.com>
2019-01-08 11:31:15 +0100

    Use spread operator to make it easier to diagnose errors.
    More idiomatic spock test

--- src/test/groovy/appgenerator/ProfileServiceSpec.groovy
+++ src/test/groovy/appgenerator/ProfileServiceSpec.groovy
@@ -2,24 +2,39 @@ package appgenerator
 
 import appgenerator.profile.Feature
 import appgenerator.profile.Profile
+import com.sun.org.apache.xpath.internal.operations.String
 import grails.testing.services.ServiceUnitTest
 import spock.lang.Specification
 
 class ProfileServiceSpec extends Specification implements ServiceUnitTest<ProfileService> {
 
-    void "test get profiles"() {
+    void "test getProfiles for 3.2.2 retrieves web, rest-api, angular2 and angular"() {
+        given:
+        Set<String> expectedProfileNames = ['web', 'rest-api', 'angular2', 'angular']
+
         when:
         List<Profile> profiles = service.getProfiles("3.2.2")
 
         then:
-        profiles.size() == 4
+        profiles*.name.size() == expectedProfileNames.size()
     }
 
     void "test get features"() {
+        Set<String> expectedFeatures = ['asset-pipeline',
+                                        'hibernate4',
+                                        'hibernate5',
+                                        'json-views',
+                                        'less-asset-pipeline',
+                                        'markup-views',
+                                        'mongodb',
+                                        'neo4j',
+                                        'rx-mongodb',
+                                        'security']
+
         when:
         List<Feature> features = service.getFeatures("3.2.2", "angular2")
 
         then:
-        features.size() == 10
+        features*.name.size() == expectedFeatures.size()
     }
 }
