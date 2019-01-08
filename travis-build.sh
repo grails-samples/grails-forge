23e1c0a1af54e63f867a32210618f7050e96c3d5
sdelamo <sergio.delamo@softamo.com>
2019-01-08 12:29:07 +0100

    Use ya-cf-app-gradle-plugin Gradle plugin

--- travis-build.sh
+++ travis-build.sh
@@ -6,10 +6,9 @@ export EXIT_STATUS=0
 ./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS ]]; then
-
     if [[ -n $TRAVIS_TAG ]]; then
         echo "Publishing to PWS"
-        ./gradlew --no-daemon  -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush || EXIT_STATUS=$?
+        ./gradlew cf-push
 
     fi
 fi
