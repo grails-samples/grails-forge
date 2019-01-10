ed785268192131a7e2d2081d92ec3246344d8d91
sdelamo <sergio.delamo@softamo.com>
2019-01-10 16:00:48 +0100

    Enable override of all properties through config

--- grails-app/services/appgenerator/CleanupJobService.groovy
+++ grails-app/services/appgenerator/CleanupJobService.groovy
@@ -1,5 +1,7 @@
 package appgenerator
 
+import grails.config.Config
+import grails.core.support.GrailsConfigurationAware
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Value
@@ -7,33 +9,42 @@ import org.springframework.scheduling.annotation.Scheduled
 
 @CompileStatic
 @Slf4j
-class CleanupJobService {
+class CleanupJobService implements GrailsConfigurationAware {
 
     boolean lazyInit = false
 
-    @Value('${TEMP_DIR_TO_CLEANUP:/home/vcap/tmp}')
-    private final String TEMP_DIR_TO_CLEANUP
+    String cleanupDir
 
-    private static final String GROOVY_TMP_DIR = 'groovy-generated'
-    private static final int MINUTES_TO_PURGE = 1
+    String groovyTmpDir
+
+    Integer threshold
+
+    @Override
+    void setConfiguration(Config co) {
+        cleanupDir = co.getProperty('cleanup.tmpdir', String, '/home/vcap/tmp')
+        groovyTmpDir = co.getProperty('cleanup.groovytmpdir', String, 'groovy-generated')
+        threshold = co.getProperty('cleanup.threshold', Integer, 1)
+    }
 
     @Scheduled(initialDelay = 10_000L, fixedDelay = 60_000L)
     void removeTemporalDirectories() {
         log.info "Cleanup job started ${new Date()}"
-        long purgeTime = System.currentTimeMillis() - MINUTES_TO_PURGE * 60 * 1000;
+        long purgeTime = System.currentTimeMillis() - threshold * 60 * 1000
 
-        File dir = new File(TEMP_DIR_TO_CLEANUP)
+        File dir = new File(cleanupDir)
         if (!dir.exists()) {
-            log.error "The temporal directory ${TEMP_DIR_TO_CLEANUP} doesn't exist"
+            log.error "The temporal directory ${cleanupDir} doesn't exist"
             return
         }
 
         // Get directories older than MINUTES_TO_PURGE and delete them
-        dir.eachDirMatch(~/${GROOVY_TMP_DIR}.*/) { File tmpDir ->
+        dir.eachDirMatch(~/${groovyTmpDir}.*/) { File tmpDir ->
             if (tmpDir.lastModified() < purgeTime) {
-                tmpDir.deleteDir()
-                log.info "Deleting temporal directory ${tmpDir.absolutePath}"
+                boolean deletionResult = tmpDir.deleteDir()
+                log.info "Deleting temporal directory ${tmpDir.absolutePath} with result ${deletionResult}"
             }
         }
     }
+
+
 }
