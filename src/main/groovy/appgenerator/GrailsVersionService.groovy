835aeccddf48d8568ff3f31d16b6bfedebff7369
Puneet Behl <behlp@objectcomputing.com>
2021-10-12 21:04:42 +0530

    Update Grails Forge to list Grails 5

--- src/main/groovy/appgenerator/GrailsVersionService.groovy
+++ src/main/groovy/appgenerator/GrailsVersionService.groovy
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 class GrailsVersionService {
 
     final static GrailsVersion LOWEST_32X = GrailsVersion.build("3.2.2")
-    final static GrailsVersion HIGHEST_40X = GrailsVersion.build("4.0.99")
+    final static GrailsVersion HIGHEST_40X = GrailsVersion.build("5.0.99")
     final static String MAVEN_METADATA= 'https://repo.grails.org/grails/core/org/grails/grails-core/maven-metadata.xml'
     final static String MAVEN_METADATA_OSS = 'https://repo1.maven.org/maven2/org/grails/grails-core/maven-metadata.xml'
 
