78187688267386331d5b3f3acacdf84f16599e31
Puneet Behl <behlp@objectcomputing.com>
2022-01-19 01:45:46 +0530

    Update Grails Forge test to point to 5.1.2

--- src/integration-test/groovy/appgenerator/VersionsControllerIntegrationSpec.groovy
+++ src/integration-test/groovy/appgenerator/VersionsControllerIntegrationSpec.groovy
@@ -53,6 +53,6 @@ class VersionsControllerIntegrationSpec extends Specification {
         List<String> versions = result.collect { it.version } as List<String>
 
         then:
-        versions.containsAll(['5.1.1'])
+        versions.containsAll(['5.1.2'])
     }
 }
