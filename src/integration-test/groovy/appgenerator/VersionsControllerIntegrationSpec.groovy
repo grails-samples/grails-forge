c2aee42d9d5becf6ea9d31d405c0c8b0446e2eff
Puneet Behl <behlp@objectcomputing.com>
2022-08-03 11:05:50 +0530

    Revert "Downgrade Grails Forge to 5.1.9"
    This reverts commit 7f8f6a7aa664533e85975294526adbfbb9c24f36.

--- src/integration-test/groovy/appgenerator/VersionsControllerIntegrationSpec.groovy
+++ src/integration-test/groovy/appgenerator/VersionsControllerIntegrationSpec.groovy
@@ -53,6 +53,6 @@ class VersionsControllerIntegrationSpec extends Specification {
         List<String> versions = result.collect { it.version } as List<String>
 
         then:
-        versions.containsAll(['5.1.8'])
+        versions.containsAll(['5.2.1'])
     }
 }
